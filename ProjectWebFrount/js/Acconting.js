f8902e97454767ed84ccb4fc07cb4a4fdc10a20e
Abolfazl Karbakhsh <110627911+AbolfazlKarbakhsh@users.noreply.github.com>
2023-03-19 21:05:31 +0330

    add js page login
    -- js ConfirmNumber And CountDown

--- ProjectWebFrount/js/Acconting.js
+++ ProjectWebFrount/js/Acconting.js
@@ -1,8 +1,60 @@
-function addConfirm(){
+
+var timerTag = document.getElementById('timer');
+var confirmTimer = document.getElementById('confirmTimer');
+var pTimer = document.querySelector('.pTimer');
+timerTag.innerHTML = 00 + ":" + 05;
+
+
+
+
+function addConfirm(e) {
+    e.pr
     var confirmTag = document.querySelector(".confirm");
+    var checkbox = document.querySelector(".checkbox");
+
     confirmTag.classList.remove("d-none");
+    checkbox.classList.add("d-none");
+    confirmTimer.classList.remove("d-none");
+
+
+    console.log(checkbox);
     submit.value = "ورود";
-    submit.setAttribute("title","وارد شوید")
+    submit.setAttribute("title", "وارد شوید");
+
+    startTimer();
+    var t = setInterval(startTimer, 1000);
+
 }
 var submit = document.querySelector("#do_login");
-submit.addEventListener('click',addConfirm)
+submit.addEventListener('click', addConfirm);
+
+
+
+
+
+function startTimer() {
+
+    var presentTime = document.getElementById('timer').innerHTML;
+    var timeArray = presentTime.split(/[:]+/);
+    var m = timeArray[0];
+    var s = checkSecond((timeArray[1] - 1));
+    if (s == 59) { m = m - 1 }
+    if (m == -1) {
+        pTimer.outerHTML = `
+        <p class='field pTimer' id="confirmTimer">
+        <label>کد تایید دوباره برای شما ارسال شد </label>
+        </p>
+        `
+        pTimer.classList.add("d-none");
+    }
+
+    document.getElementById('timer').innerHTML =
+        m + ":" + s;
+}
+
+
+function checkSecond(sec) {
+    if (sec < 10 && sec >= 0) { sec = "0" + sec }; // add zero in front of numbers < 10
+    if (sec < 0) { sec = "59" };
+    return sec;
+}
